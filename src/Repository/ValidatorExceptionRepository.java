6e592d529045da9bbeca121647a7955045b8193f
Cristina Szakacs <szakacscristina94@yahoo.com>
2019-03-30 10:47:25 +0200

    Searches full text

---
+++ src/Repository/ValidatorExceptionRepository.java
@@ -0,0 +1,10 @@
+package Repository;
+import java.lang.Exception;
+import java.lang.RuntimeException;
+
+    public class ValidatorExceptionRepository extends RuntimeException {
+        public ValidatorExceptionRepository (String msg){
+            super(msg);
+        }
+    }
+
--- src/Service/IsSearchable.java
+++ src/Service/IsSearchable.java
@@ -1,4 +1,3 @@
-
 package Service;
 
 import Domain.Entity;
